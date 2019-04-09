d8f7aa3540606c141365a6ebefc96d4c411410f4
ju1 <j.fedorovicius@gmail.com>
2019-04-09 10:42:39 +0300

    Added support to the embedded Google Translate feature
    Some website might contain a dropdown menu offering a translation language. This update will support that as well

--- page-translations-listener.js
+++ page-translations-listener.js
@@ -1,53 +1,56 @@
 (function() {
 // Observe DOM mutations whether the <html> node was changed by Google Translate
 if (window.MutationObserver) {
-	var mutationObserver = new MutationObserver(function(mutations) {
-		mutations.forEach(function (mutation) {
-			var oldElementClass = mutation.oldValue;
-			var currentElementClass = mutation.target.className;
-			if (oldElementClass.indexOf('translated-') === -1 && currentElementClass.indexOf('translated-') > -1) {
-				window.dataLayer.push({
-					'event' : 'pageTranslated',
-					'translationLanguage' : mutation.target.lang,
-					'translationService' : 'google translate in chrome'
-				});
-			}
-		})
-	}) 
+  var mutationObserver = new MutationObserver(function(mutations) {
+    mutations.forEach(function (mutation) {
+      var oldElementClass = mutation.oldValue;
+      var currentElementClass = mutation.target.className;
+      if (oldElementClass.indexOf('translated-') === -1 && currentElementClass.indexOf('translated-') > -1) {
+		console.log(mutation);
+        window.dataLayer.push({
+          'event' : 'pageTranslated',
+          'translationLanguage' : mutation.target.lang || document.getElementsByTagName('html')[0].getAttribute('xml:lang'),
+          'translationService' : 'on-page google translate'
+        });
+      }
+  })
+})
 
-	var htmlNode = document.querySelector('html');
-	mutationObserver.observe(htmlNode, {
-		attributes: true,
-		attributeOldValue: true,
-		attributeFilter: ['class']
-	})
-	
+  var htmlNode = document.querySelector('html');
+  mutationObserver.observe(htmlNode, {
+    attributes: true,
+    attributeOldValue: true,
+    attributeFilter: ['class']
+  })
+
 }
+
+
 
 // Let's also track pageviews when the page is translated directly from translate.google.com or bing.com/translator
 // A function that can return individual query parameter (borrowed from https://davidwalsh.name/query-string-javascript)
 function getUrlParameter(name) {
-    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
-    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
-    var results = regex.exec(location.search);
-    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+  name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+  var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+  var results = regex.exec(location.search);
+  return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
 };
 
 // Check if the page is being translated directly from translate.google.com (viewed within the iframe)
 if (window.location.href.indexOf('translate.googleusercontent.com') > -1 ) {
-	window.dataLayer.push({
-		'event' : 'pageTranslated',
-		'translationLanguage' : getUrlParameter('tl'),
-		'translationService' : 'google translate website'
-	});
+  window.dataLayer.push({
+    'event' : 'pageTranslated',
+    'translationLanguage' : getUrlParameter('tl'),
+    'translationService' : 'google translate website'
+  });
 }
 
 // Check if the page is being translated directly from bing.com/translator (viewed within the iframe)
 if (window.location.href.indexOf('translatoruser-int.com') > -1 ) {
-	window.dataLayer.push({
-		'event' : 'pageTranslated',
-		'translationLanguage' : getUrlParameter('to'),
-		'translationService' : 'bing translator website'
-	});
+  window.dataLayer.push({
+    'event' : 'pageTranslated',
+    'translationLanguage' : getUrlParameter('to'),
+    'translationService' : 'bing translator website'
+  });
 }
 })();
